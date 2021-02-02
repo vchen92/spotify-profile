a0350bd2a75d91e7b5e6c26a46cf41ff02f78998
Vincent Chen <vchen92@users.noreply.github.com>
2021-02-01 19:23:35 -0800

    get response token

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import Login from './components/Login/Login';
+import { getTokenFromUrl } from './spotify';
 import './App.css';
 
 function App() {
-	return (
-		<div className="app">
-			<h1>Spotify Clone app</h1>
-		</div>
-	);
+	const [token, setToken] = useState(null);
+
+	useEffect(() => {
+		const hash = getTokenFromUrl();
+		window.location.hash = '';
+
+		const _token = hash.access_token;
+
+		if (_token) {
+			setToken(_token);
+		}
+	}, []);
+
+	return <div className="app">{token ? <h1>Logged In</h1> : <Login />}</div>;
 }
 
 export default App;
