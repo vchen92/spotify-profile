1aa23d5c1ffa024af82fba45e99ef794a1e073b1
Vincent Chen <vchen92@users.noreply.github.com>
2021-02-04 15:50:32 -0800

    datalayer complete

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import { getTokenFromUrl } from './spotify';
 import './App.css';
 import SpotifyWebApi from 'spotify-web-api-js';
 import Player from './components/Player/Player';
+import { useDataLayerValue } from './hoc/DataLayer/DataLayer';
 
 const spotify = new SpotifyWebApi();
 
 function App() {
-	const [token, setToken] = useState(null);
+	const [{ user, token }, dispatch] = useDataLayerValue();
 
 	useEffect(() => {
 		const hash = getTokenFromUrl();
@@ -17,16 +18,22 @@ function App() {
 		const _token = hash.access_token;
 
 		if (_token) {
-			setToken(_token);
+			dispatch({ type: 'SET_TOKEN', token: _token });
 
 			spotify.setAccessToken(_token);
+
 			spotify.getMe().then(user => {
 				console.log('person', user);
+				dispatch({ type: 'SET_USER', user });
 			});
 		}
 	}, []);
 
-	return <div className="app">{token ? <Player /> : <Login />}</div>;
+	return (
+		<div className="app">
+			{token ? <Player spotify={spotify} /> : <Login />}
+		</div>
+	);
 }
 
 export default App;
--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -3,20 +3,27 @@ export const initialState = {
 	playlists: [],
 	playing: false,
 	item: null,
+	token:
+		'BQBr9Dgxw2iQp_OepJFH17cfXiwcVstKbpN70w-qj3Ms4SynUp…ZLR5qUqJRIJ7N_C0pMnbgcx2ktm0uEWJtT4dEI95LhjokT7C_',
 };
 
 const reducer = (state, action) => {
-  console.log(action);
+	console.log(action);
 
-  switch(action.type) {
-    case 'SET_USER':
-      return {
-        ...state,
-        user: action.user
-      }
-    default:
-      return state;
-  }
-}
+	switch (action.type) {
+		case 'SET_USER':
+			return {
+				...state,
+				user: action.user,
+			};
+		case 'SET_TOKEN':
+			return {
+				...state,
+				token: action.token,
+			};
+		default:
+			return state;
+	}
+};
 
 export default reducer;
