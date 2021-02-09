5f601a245c29862fb2a1bf11aba47a7c52855e87
Vincent Chen <vchen92@users.noreply.github.com>
2021-02-09 11:28:39 -0800

    controls completed

--- src/components/PlayerControls/PlayerControls.js
+++ src/components/PlayerControls/PlayerControls.js
@@ -1,10 +1,48 @@
 import React from 'react';
 import './PlayerControls.css';
 
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
+import VolumeDownIcon from '@material-ui/icons/VolumeDown';
+import { Grid, Slider } from '@material-ui/core';
+
 function PlayerControls() {
 	return (
 		<div className="playerControls">
-			<h1>I am the footer</h1>
+			<div className="playerControls__left">
+				<img className="playerControls__albumLogo" src="" alt="" />
+				<div className="playerControls__songInfo">
+					<h4>Yeah!</h4>
+					<p>Usher</p>
+				</div>
+			</div>
+			<div className="playerControls__center">
+				<ShuffleIcon className="playerControls__green" />
+				<SkipPreviousIcon className="playerControls__icon" />
+				<PlayCircleOutlineIcon
+					fontSize="large"
+					className="playerControls__icon"
+				/>
+				<SkipNextIcon className="playerControls__icon" />
+				<RepeatIcon className="playerControls__green" />
+			</div>
+			<div className="playerControls__right">
+				<Grid container spacing={2}>
+					<Grid item>
+						<PlaylistPlayIcon />
+					</Grid>
+					<Grid item>
+						<VolumeDownIcon />
+					</Grid>
+					<Grid item xs>
+						<Slider />
+					</Grid>
+				</Grid>
+			</div>
 		</div>
 	);
 }
--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -3,7 +3,8 @@ export const initialState = {
 	playlists: [],
 	playing: false,
 	item: null,
-	token: null,
+	token:
+		'BQBr9Dgxw2iQp_OepJFH17cfXiwcVstKbpN70w-qj3Ms4SynUp…ZLR5qUqJRIJ7N_C0pMnbgcx2ktm0uEWJtT4dEI95LhjokT7C_',
 };
 
 const reducer = (state, action) => {
