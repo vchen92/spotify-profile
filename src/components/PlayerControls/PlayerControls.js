c2afed47c23204c835f9369511cc3fcc61f03340
Vincent Chen <vchen92@users.noreply.github.com>
2021-02-25 14:07:28 -0800

    play pause and skip

--- src/components/PlayerControls/PlayerControls.js
+++ src/components/PlayerControls/PlayerControls.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './PlayerControls.css';
 
 import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import PauseCircleOutlineIcon from '@material-ui/icons/PauseCircleOutline';
 import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
 import SkipNextIcon from '@material-ui/icons/SkipNext';
 import ShuffleIcon from '@material-ui/icons/Shuffle';
@@ -9,27 +10,125 @@ import RepeatIcon from '@material-ui/icons/Repeat';
 import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
 import VolumeDownIcon from '@material-ui/icons/VolumeDown';
 import { Grid, Slider } from '@material-ui/core';
+import { useDataLayerValue } from './../../hoc/DataLayer/DataLayer';
 
-function PlayerControls() {
+function PlayerControls({ spotify }) {
+	const [{ item, playing }, dispatch] = useDataLayerValue();
+	const [value, setValue] = useState(30);
+
+	const handleSliderChange = (event, newValue) => {
+		setValue(newValue);
+		console.log(newValue);
+		spotify.setVolume(value);
+	};
+
+	useEffect(() => {
+		spotify.getMyCurrentPlaybackState().then(r => {
+			dispatch({
+				type: 'SET_PLAYING',
+				playing: r.is_playing,
+			});
+
+			dispatch({
+				type: 'SET_ITEM',
+				item: r.item,
+			});
+		});
+	}, [spotify, dispatch]);
+
+	const handlePlayPause = () => {
+		if (playing) {
+			spotify.pause();
+			dispatch({
+				type: 'SET_PLAYING',
+				playing: false,
+			});
+		} else {
+			spotify.play();
+			dispatch({
+				type: 'SET_PLAYING',
+				playing: true,
+			});
+		}
+	};
+
+	const skipNext = () => {
+		spotify.skipToNext();
+		spotify.getMyCurrentPlayingTrack().then(r => {
+			dispatch({
+				type: 'SET_ITEM',
+				item: r.item,
+			});
+			dispatch({
+				type: 'SET_PLAYING',
+				playing: true,
+			});
+		});
+	};
+
+	const skipPrevious = () => {
+		spotify.skipToPrevious();
+		spotify.getMyCurrentPlayingTrack().then(r => {
+			dispatch({
+				type: 'SET_ITEM',
+				item: r.item,
+			});
+			dispatch({
+				type: 'SET_PLAYING',
+				playing: true,
+			});
+		});
+	};
+
 	return (
 		<div className="playerControls">
 			<div className="playerControls__left">
-				<img className="playerControls__albumLogo" src="" alt="" />
-				<div className="playerControls__songInfo">
-					<h4>Yeah!</h4>
-					<p>Usher</p>
-				</div>
+				<img
+					className="playerControls__albumLogo"
+					src={item?.album.images[0].url}
+					alt={item?.name}
+				/>
+				{item ? (
+					<div className="playerControls__songInfo">
+						<h4>{item.name}</h4>
+						<p>
+							{item.artists.map(artist => artist.name).join(', ')}
+						</p>
+					</div>
+				) : (
+					<div className="playerControls__songInfo">
+						<h4>No song is playing</h4>
+						<p>...</p>
+					</div>
+				)}
 			</div>
+
 			<div className="playerControls__center">
 				<ShuffleIcon className="playerControls__green" />
-				<SkipPreviousIcon className="playerControls__icon" />
-				<PlayCircleOutlineIcon
-					fontSize="large"
+				<SkipPreviousIcon
+					onClick={skipNext}
 					className="playerControls__icon"
 				/>
-				<SkipNextIcon className="playerControls__icon" />
+				{playing ? (
+					<PauseCircleOutlineIcon
+						onClick={handlePlayPause}
+						fontSize="large"
+						className="footer__icon"
+					/>
+				) : (
+					<PlayCircleOutlineIcon
+						onClick={handlePlayPause}
+						fontSize="large"
+						className="footer__icon"
+					/>
+				)}
+				<SkipNextIcon
+					onClick={skipPrevious}
+					className="playerControls__icon"
+				/>
 				<RepeatIcon className="playerControls__green" />
 			</div>
+
 			<div className="playerControls__right">
 				<Grid container spacing={2}>
 					<Grid item>
@@ -39,7 +138,12 @@ function PlayerControls() {
 						<VolumeDownIcon />
 					</Grid>
 					<Grid item xs>
-						<Slider />
+						<Slider
+							value={value}
+							onChange={(e, val) => setValue(val)}
+							onChangeCommitted={handleSliderChange}
+							aria-labelledby="continuous-slider"
+						/>
 					</Grid>
 				</Grid>
 			</div>
